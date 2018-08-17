575913c74fbced2d64c1c749af01a3fb8e1bb143
heedypo <andy.dawes@live.co.uk>
2018-08-17 16:08:35 +0100

    ~ .tmux.conf (changed colours in status line)

--- tmux/muxa.sh
+++ tmux/muxa.sh
@@ -16,43 +16,15 @@ tmux send-keys -t all:3 'ranger' C-m
 
 tmux new-window -n read -t all
 tmux send-keys -t all:4 'newsbeuter' C-m
-tmux split-window -v -p 50 -t all	
-tmux send-keys -t all:4.2 'podbeuter' C-m 
 
-tmux new-window -n talk -t all
-tmux send-keys -t all:5 'finch' C-m
-tmux split-window -h -p 25 -t all	
-tmux send-keys -t all:5.2 'rad' C-m 
-
-tmux new-window -n ask -t all
-tmux send-keys -t all:6 'weechat-curses' C-m
-tmux split-window -h -p 50 -t all
-tmux send-keys -t all:6.2 'rtv' C-m
+#tmux new-window -n ask -t all
+#tmux send-keys -t all:6 'weechat-curses' C-m
 
 tmux new-window -n check -t all
-tmux send-keys -t all:7 'htop' C-m
-tmux split-window -v -p 33 -t all
-tmux send-keys -t all:7.2 'rtorrent' C-m
+tmux send-keys -t all:5 'htop' C-m
 
 tmux select-window -t all:2
 
 fi
 
 tmux attach -t all
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
